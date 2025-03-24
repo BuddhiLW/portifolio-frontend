dd2672b72db96fa4ec57ca9a4bce2606eb154019
Pedro Gomes <pedrogbranquinho@gmail.com>
2025-03-24 09:05:06 -0300

    feat: get technologies through API

--- src/functions/api.ts
+++ src/functions/api.ts
@@ -1,6 +1,13 @@
 const baseURL = process.env.NEXT_PUBLIC_BASE_URL
 
 export async function httpGet(url: string) {
-  const response = await fetch(url)
+  const response = await fetch(normalizeUrl(`${baseURL}${url}`))
   return await response.json()
+}
+
+function normalizeUrl(url: string) {
+  const protocol = url.split("://")[0]
+  const rest = url.split("://")[1]
+
+  return `${protocol}://${rest.replaceAll(/\/{2,}/g, "/")}`
 }
---
+++ src/functions/tecnologias.ts
@@ -0,0 +1,12 @@
+import { Tecnologia } from "@core"
+import { httpGet } from "./api"
+
+export async function getTech() {
+  const techs: Tecnologia[] = await httpGet("/tecnologias")
+  return {
+    all: techs,
+    get featuring() {
+      return techs.filter((tech) => tech.destaque)
+    },
+  }
+}
