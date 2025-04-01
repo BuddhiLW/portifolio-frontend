df162bc0cd01f25473e108b7a747e3a0c9a2cbcf
Pedro Gomes <pedrogbranquinho@gmail.com>
2025-04-01 14:34:11 -0300

    fix: next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,32 +4,23 @@ import type { NextConfig } from "next"
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts")
 
 const nextConfig: NextConfig = {
-	/* config options here */
-	images: {
-		dangerouslyAllowSVG: true,
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "**",
-			},
-		],
-		domains: ["github.com"],
-	},
-	experimental: {
-		serverComponentsExternalPackages: ["next"],
-	},
-	// Increase API timeout limit for all routes
-	api: {
-		bodyParser: {
-			sizeLimit: '1mb',
-		},
-		responseLimit: false,
-	},
-	// Increase serverside timeout for requests
-	serverRuntimeConfig: {
-		// Will only be available on the server side
-		timeoutMs: 60000, // 60 seconds
-	},
+  /* config options here */
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+    domains: ["github.com"],
+  },
+  serverComponentsExternalPackages: ["next"],
+  // Increase serverside timeout for requests
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    timeoutMs: 60000, // 60 seconds
+  },
 }
 
 export default withNextIntl(nextConfig)
